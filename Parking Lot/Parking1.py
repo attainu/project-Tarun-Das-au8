4cb29c4cf5b94205f9d0bcf5d23b17d4a66c77b1
Tarun Das <imtarun79@gmail.com>
2020-11-15 22:34:43 +0530

    Python Project on Parking lot

---
+++ Parking Lot/Parking1.py
@@ -0,0 +1,10 @@
+class Vehicle:
+	def __init__(self,regno,color):
+		self.color =  color
+		self.regno = regno
+
+class Car(Vehicle):
+
+	def __init__(self,regno,color):
+		Vehicle.__init__(self,regno,color)
+
